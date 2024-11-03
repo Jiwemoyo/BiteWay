0a46ba9f79fce20bc867f8d31b5635577a5de07b
Aless4ndr0 <jiwemoyo@gmail.com>
2024-11-02 20:43:47 -0500

    agregando enlaces

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,28 @@
-import React from "react";
-import { FaFacebook, FaTwitter, FaUtensils } from "react-icons/fa"; // Íconos de Font Awesome
-import "../styles/Home.css";
+<section className="home">
+  <img src="bitewayicon.png" alt="biteWayLogo" className="biteWayLogo-home" />
+  <div className="buttons-contact-home">
+    <button className="button-contact">Contáctanos</button>
+    <ul className="social-links">
+      <li>
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebook size={35} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter size={35} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.ubereats.com" target="_blank" rel="noopener noreferrer">
+          <FaUtensils size={35} />
+        </a>
+      </li>
+    </ul>
+  </div>
 
-export const Home = () => {
-  return (
-    <section className="home">
-      <img src="bitewayicon.png" alt="BiteWay Logo" className="biteWayLogo-home" />
-      
-      <div className="contact-section">
-        <button className="button-contact">Contáctanos</button>
-        
-        <ul className="social-links">
-          <li>
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-              <FaFacebook size={35} /> {/* Tamaño ajustable */}
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-              <FaTwitter size={35} />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.ubereats.com" target="_blank" rel="noopener noreferrer">
-              <FaUtensils size={35} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </section>
-  );
-};
+  {/* Añadir los tres enlaces aquí */}
+  <a href="#" className="link-left">Enlace Izquierda</a>
+  <a href="#" className="link-right">Enlace Derecha</a>
+  <a href="#" className="link-bottom">Enlace Abajo</a>
+</section>
